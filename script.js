a54a4293ed46ffc8d72591db56e1089db188f16a
01aaa01 <abdunnurmusoxonov213@gmail.com>
2025-03-11 19:41:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism = prompt('Ismingizni kiriting')
+let familiya = prompt('Familiyangizni kiriting')
+let yosh = prompt('Yoshingizni kiriting')
+let fan = prompt('Yoqtirgan faningizni yozing')
+let Tuman = prompt('Yashaydigan tumaningizni kiriting')
+
+console.log('Ism:' + ism);
+console.log('Familiya:' + familiya);
+console.log('Yosh:' + yosh);
+console.log('Yoqtirgan fan:' + fan);
+console.log('Yashash manzili:' + Tuman);
+
+
+
